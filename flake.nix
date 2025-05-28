4b04018fd8f1db353f104c8adcc58f7835747c1a
serpro69 <22973227+serpro69@users.noreply.github.com>
2025-05-28 18:09:08 +0200

    Add nix flake with some basics

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Ktchn8s Lab Shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          packages = [
+            diffutils
+            git
+            jq
+            fzf
+            tmux
+            neovim
+            openssh
+            python3
+
+            (python3.withPackages (p: with p; [
+              pip
+              mkdocs
+              mkdocs-material
+            ]))
+          ];
+
+          shellHook = ''
+            # can run stuff here
+          '';
+        };
+      }
+    );
+}
