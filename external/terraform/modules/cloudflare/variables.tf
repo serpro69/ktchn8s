ed7d6536b65ef1f9dfbf29acde20b22df4f95990
serpro69 <22973227+serpro69@users.noreply.github.com>
2025-07-20 15:07:41 +0200

    Add var for cloudflare domain name

--- external/terraform/modules/cloudflare/variables.tf
+++ external/terraform/modules/cloudflare/variables.tf
@@ -7,3 +7,8 @@ variable "cloudflare_zone_id" {
   type      = string
   sensitive = true
 }
+
+variable "cloudflare_domain" {
+  type        = string
+  description = "The domain name to use for the applications"
+}
