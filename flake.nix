9a03d401674c8349725a4e76632b656f98850e1c
serpro69 <22973227+serpro69@users.noreply.github.com>
2026-02-09 20:11:33 +0100

    Update nixos to 25.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "ktchn8s Lab Shell";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -60,7 +60,7 @@
             # k8s tools
             dyff
             k9s
-            kube3d
+            k3d
             kubectl
             kubernetes-helm
             kustomize
